4c79c0dd422f41fabb99ef9245611b8cedfda97d
lingruiluo <ll3356@columbia.edu>
2020-06-16 11:01:19 -0400

    initial

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+url = ""
+version = "0.1.0"
+readme = open('README.md').read()
+
+setup(
+    name="jp_turtle",
+    packages=["jp_turtle"],
+    version=version,
+    description="Simple jupyter widget wrappers for turtle implementation",
+    long_description=readme,
+    include_package_data=True,
+    author="Aaron Watters; Lingrui Luo",
+    author_email="awatters@flatironinstitute.org; ll3356@columbia.edu",
+    url=url,
+    install_requires=[
+        "jp_proxy_widget", 
+        #"requests", 
+        #"matplotlib", 
+        #"numpy", 
+        #"scipy", 
+        "pillow", 
+        #"psutil",
+        "imageio",
+        #"sklearn",
+        "jupyter-ui-poll",
+        ],
+    license="MIT"
+)
